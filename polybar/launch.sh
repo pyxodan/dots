3bee9b784b62e69f1ed300297319822d89031bd6
Ankush Ahuja <ahujaankush595@gmail.com>
2022-05-01 22:39:15 +0200

    upload

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
